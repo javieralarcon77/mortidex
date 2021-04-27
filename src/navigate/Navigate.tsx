e2798389fb343a907a749fa93c9009fe0d581b0f
Javier Alarcon <javier.alarcon@pragma.com.co>
2021-04-27 17:07:45 -0500

    actualizado info del navigate

--- src/navigate/Navigate.tsx
+++ src/navigate/Navigate.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import PeopleScreen from '../screens/PeopleScreen';
 
 const Stack = createStackNavigator();
 
 export const Navigate = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown:false,
+                cardStyle:{
+                    backgroundColor:'white',
+                }
+            }}
+        >
             <Stack.Screen name="HomeScreen" component={ HomeScreen }/>
+            <Stack.Screen name="PeopleScreen" component={ PeopleScreen }/>
         </Stack.Navigator>
     )
 }
